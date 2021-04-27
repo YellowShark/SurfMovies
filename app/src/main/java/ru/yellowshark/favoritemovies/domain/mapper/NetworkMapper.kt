6f16b711401a0885c71049bf2d9c3faabd99e61b
Daniil Dobrodeev <danil.dobrodeev@mail.ru>
2021-04-27 22:09:14 +0300

    change date format

--- app/src/main/java/ru/yellowshark/favoritemovies/domain/mapper/NetworkMapper.kt
+++ app/src/main/java/ru/yellowshark/favoritemovies/domain/mapper/NetworkMapper.kt
@@ -3,6 +3,25 @@ package ru.yellowshark.favoritemovies.domain.mapper
 import ru.yellowshark.favoritemovies.data.network.response.MovieResult
 import ru.yellowshark.favoritemovies.domain.model.Movie
 import ru.yellowshark.favoritemovies.utils.Mapper
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+private fun String.toRussianFormat():String {
+    var sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    var date: Date? = null
+    try {
+        date = sdf.parse(this)
+    } catch (e: ParseException) {
+        e.printStackTrace()
+    }
+    var millis: Long = 0
+    if (date != null) {
+        millis = date.time
+    }
+    sdf = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+    return sdf.format(Date(millis))
+}
 
 class NetworkMapper : Mapper<MovieResult, Movie> {
     override fun toDomain(dto: MovieResult) =
@@ -11,7 +30,7 @@ class NetworkMapper : Mapper<MovieResult, Movie> {
             title = dto.title,
             desc = dto.overview,
             image = dto.posterPath ?: "",
-            releaseDate = dto.releaseDate,
+            releaseDate = dto.releaseDate.toRussianFormat(),
             isLiked = false
         )
 
